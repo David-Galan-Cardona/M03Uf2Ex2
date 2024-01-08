a7e1587cc3c40e1011f727f451f7697d9f9ca305
David Galan <david.galan.7e7@itb.cat>
2024-01-08 13:52:40 +0100

    Added power. Closes #2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,13 @@
 ﻿using System;
-namespace M3UF2
+
+namespace M03.UF2
 {
     class Ex2
     {
         public static void Main()
         {
             const string Choice = "1.Validar si un nombre és senar \n2.Calcular la potència d'un nombre \n3.Retornar un valor aleatori \n4.Comptar el nombre de vocals o consonants en un text " +
-                "\n5.Sortir", Question = "Què vols fer?", NotValid = "Introdueix un nombre vàlid", Odd = "El nombre és senar", NotOdd = "El nombre no és senar";
+                "\n5.Sortir", Question = "Què vols fer?", NotValid = "Introdueix un nombre vàlid", Odd = "El nombre és senar", NotOdd = "El nombre no és senar", Goodbye = "Fins aviat";
             int choice;
 
             Console.WriteLine(Question);
@@ -27,6 +28,14 @@ namespace M3UF2
                 {
                     Console.WriteLine(NotOdd);
                 }
+            }
+            if (choice == 2)
+            {
+                Console.WriteLine(Power());
+            }
+            if (choice == 5)
+            {
+                Console.WriteLine(Goodbye);
             }
 
         }
@@ -55,6 +64,28 @@ namespace M3UF2
             {
                 return true;
             }
+        }
+        public static int Power()
+        {
+            const string Base = "Digues la base", Exp = "Digues l'exponent";
+            int bas, exp = -1, result = 1;
+
+            Console.WriteLine(Base);
+            bas = Validate();
+            while (exp < 0)
+            {
+                Console.WriteLine(Exp);
+                exp = Validate();
+            }
+            if (exp == 0)
+            {
+                return 1;
+            }
+            for (int i = 0; i < exp; i++)
+            {
+                result *= bas;
+            }
+            return result;
         }
     }
 }
